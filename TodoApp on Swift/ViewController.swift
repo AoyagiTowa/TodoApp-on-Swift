ac734598033c97dc2ec52420a6331454e375f127
TowaAoyagi <tab.git.kamui@gmail.com>
2021-02-15 22:16:21 +0900

    test for datepicker

--- TodoApp on Swift/ViewController.swift
+++ TodoApp on Swift/ViewController.swift
@@ -8,12 +8,27 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var datePicker: UIDatePicker!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        datePicker.preferredDatePickerStyle = .wheels
+        datePicker.timeZone = NSTimeZone.local
+        datePicker.datePickerMode = .date
+        datePicker.locale = Locale(identifier: "ja_JP")
+        
+        
+
+        
         // Do any additional setup after loading the view.
     }
-
-
+    
+    
+    @IBAction func get() {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyyMMdd"
+        print((formatter.string(from: datePicker.date)))
+    }
 }
 
